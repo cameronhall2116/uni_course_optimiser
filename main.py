facfe6aa36c032e3a0437053cb7ae62a18e5973d
Cameron Hall <cameronhall@Camerons-MBP.modem>
2023-05-02 10:16:29 +0800

    Add user input and fixed bugs

--- main.py
+++ main.py
@@ -2,34 +2,60 @@ from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 import re
 import pandas as pd
-from optimiser import *
 from optimiser2 import *
 
-'''req = Request(
-    url='https://handbooks.uwa.edu.au/majordetails?code=mjd-physc#dsmlevel1',
-    headers={'User-Agent': 'Mozilla/5.0'}
-)'''
+# Ask user for major course code and units completed so far
+
+course_code = ''
+valid_course = False
+while valid_course is False:
+    course_code = input("Please enter the course code for your major (it should have the structure "
+                        "MJD-PHYSC): ")
+    if len(course_code) != 9:
+        print("Invalid course code entered.")
+    else:
+        valid_course = True
+
+completed_units = ''
+valid_unit = False
+while valid_unit is False:
+    completed_units = input("Please enter the units you've completed so far, separating them "
+                            "with a comma (if none hit enter): ")
+    completed_units = completed_units.upper()
+    completed_units = completed_units.split(',')
+    for x in completed_units:
+        if len(x) != 8 and completed_units != ['']:
+            print("Invalid unit entered.")
+            valid_unit = False
+        else:
+            valid_unit = True
+
+# Format url using the course code entered
+
+course_url = "https://handbooks.uwa.edu.au/majordetails?code=" + course_code + "#dsmlevel1"
 
 req = Request(
-    url='https://handbooks.uwa.edu.au/majordetails?code=MJD-ARIDM#dsmlevel1',
+    url=course_url,
     headers={'User-Agent': 'Mozilla/5.0'}
 )
 
+# Scrape the webpage
+
 webpage = urlopen(req).read()
-
-# unit list = [{code: {availability: ,prerequisites}}]
-
-unit_list = []
-
 soup = BeautifulSoup(webpage, 'html.parser')
 table_rows = soup.find_all('tr')
+paragraphs = soup.find_all('p')
+
 count = 1
 
 del table_rows[0]
 
+unit_list = []
+
+# Scrape relevant info
+
 for i in table_rows:
     count+=1
-    print(i)
     availability_item = str(i.contents[1])
     availability = re.findall("[A-Z][0-9]", availability_item)
     unit_code_item = str(i.contents[3])
@@ -53,21 +79,15 @@ for i in table_rows:
     unit_tup = (unit_code, availability, prereq_list)
     unit_list.append(unit_tup)
 
-completed_units = ["CHEM1003", "MATH1721", "PHYS1030", "MATH1720", "MATH1722", "CITS1001"]
+completed_units = ["CHEM1003", "MATH1721", "PHYS1030", "MATH1720", "MATH1722", "CITS1001", "CITS5505"
+                   "PPHE2211", "PPHE3327", "CITS1002", "PHIL1007", "CITS3004", "SCIE2100", "CITS2401"]
+
 updated_unit_list = []
 for units in unit_list:
     if units[0] not in completed_units:
         updated_unit_list.append(units)
 
-
 df = pd.DataFrame.from_records(updated_unit_list, columns=['Unit', 'Availability', 'Prerequisites'])
-
-print(df)
-#print(updated_unit_list)
-
-
-#optimise(updated_unit_list, 'S2')
-#optimise2(updated_unit_list)
 
 opto(updated_unit_list, completed_units, 2, 2023, 4)
 
--- optimiser2.py
+++ optimiser2.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import re
 
 class Unit:
     def __init__(self, code, availability, prerequisites):
@@ -9,12 +8,16 @@ class Unit:
 
 
 def opto(units, completed_units, sem, year, courseYears):
+    #format and initialise variables
     semCode = "S" + str(sem)
     startingSem = str(year) + " S" + str(sem)
     semList = [semCode]
     semYearList = [startingSem]
     course = {startingSem: ["MATH1722"]}
 
+    temp_set = set()
+
+    # add all semesters to dictionary
     for x in range(courseYears*2):
         if sem == 2:
             year+=1
@@ -30,21 +33,45 @@ def opto(units, completed_units, sem, year, courseYears):
             semYearList.append(semester)
             course[semester] = []
 
-    for i in units:
-        unit = Unit(i[0], i[1], i[2])
+    # this loop iterates through every unit and tries to add to dictionary
+    i = 0
+    while i < len(units):
+        unit = Unit(units[i][0], units[i][1], units[i][2])
         code = unit.code
         check = all(items in completed_units for items in unit.prerequisites)
         x = 0
-        while x < (courseYears * 2)-1:
+        y = 0
+
+        # this checks if any units that previously couldn't be added due to lacking prerequisites
+        # can now be added
+        if len(temp_set) > 0:
+            temp_code = list(temp_set)[0]
+            while y < (courseYears * 2):
+                semCode = semYearList[y][len(semYearList[y]) - 1]
+                check2 = set(unit.prerequisites).isdisjoint(course[semYearList[y]])
+                if (check is True) and (check2 is True) and len(course[semYearList[y]]) < 4 \
+                        and (semCode in str(unit.availability)) and (temp_code not in completed_units):
+                    course[semYearList[y]].append(temp_code)
+                    completed_units.append(temp_code)
+                    temp_set.remove(temp_code)
+                    break
+                y += 1
+
+        # this is the main loop for adding units to dictionary
+        while x < (courseYears * 2):
             semCode = semYearList[x][len(semYearList[x])-1]
             check2 = set(unit.prerequisites).isdisjoint(course[semYearList[x]])
             if (check is True) and (check2 is True) and len(course[semYearList[x]]) < 4 \
                 and (semCode in str(unit.availability)) and (code not in completed_units):
-                #print("Adding to dict...")
                 course[semYearList[x]].append(code)
                 completed_units.append(code)
-            x+=1
+                break
+            x += 1
+        if code not in completed_units:
+            temp_set.add(code)
+        i += 1
 
+    # print the results in a pandas dataframe
     df = pd.DataFrame.from_dict(course, orient="index")
     df.transpose()
     print(df)
